7281a89c33ee254cdd685f0ddf58412030353d76
inakigarat <iniakigarat@gmail.com>
2026-03-30 00:54:20 -0300

    Add files via upload

--- casting.py
+++ casting.py
@@ -1,3 +1,18 @@
 def casting():
     """Lee precio, descuento y cantidad como texto y calcula el precio con descuento y el total."""
+
+    precio = int(input("Ingrese el precio: "))
+    descuento = float(input("Ingrese el descuento : "))
+    cantidad = int(input("Ingrese la cantidad de precio: "))
+    precio_con_descuento = precio - descuento
+    total = precio_con_descuento * cantidad
+
+    print("Precio:", precio)
+    print("Descuento:", descuento)
+    print("Precio con descuento:", precio_con_descuento)
+    print("Total:", total)
+
     pass
+
+
+casting()
--- string_methods.py
+++ string_methods.py
@@ -5,6 +5,46 @@ def string_methods():
     """
     nombre = "   Grace Hopper   "
     frase = "Python es un gran lenguaje de programacion"
-    multilinea = """Linea 1
-    Linea 2
-    Linea 3"""
+    multilinea = ("Linea 1\n"
+                  "Linea 2\n"
+                  "Linea 3\n")
+
+
+    nombre_sin_espacios = nombre.strip()
+    nombre_sin_espacios_izquierda = nombre.lstrip()
+    nombre_sin_espacios_derecha = nombre.rstrip()
+    frase_mayuscula = frase.upper()
+    frase_minuscula = frase.lower()
+    frase_titulo = frase.title()
+    posicion_gran = frase.find("gran")
+    remplazar_frase = frase.replace("programacion", "desarrollo")
+    cantidad_de_a = frase.count("a")
+    contiene_python = "Python" in frase
+    contiene_java = "Java" in frase
+
+
+
+    print("Strip:", nombre_sin_espacios)
+    print("Lstrip:", nombre_sin_espacios_izquierda)
+    print("Rstrip:", nombre_sin_espacios_derecha)
+    print("Upper:", frase_mayuscula)
+    print("Lower:", frase_minuscula)
+    print("Title:", frase_titulo)
+    print("Find:", posicion_gran)
+    print("Replace:", remplazar_frase)
+    print("Count:", cantidad_de_a)
+    print("Contiene Python:", contiene_python)
+    print("Contiene Java:", contiene_java)
+    print("Slice:", frase [0:6])
+    print("Paso:", (frase[0:6])[::2])
+    print("Reverso:", (frase [0:6])[::-1])
+    print("Formato:",f"{nombre_sin_espacios} sabe Python")
+    print(multilinea)
+
+
+
+
+string_methods()
+
+
+
